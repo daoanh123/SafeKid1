3caad1a8f42836ffe55e62016efdd5750f3fac52
daoanh123 <37476074+daoanh123@users.noreply.github.com>
2022-05-21 15:44:03 +0700

    Add project files.

---
+++ SafeKid1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SafeKid1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+       private void ButtonLogin(object sender, RoutedEventArgs e)
+        {
+            var pass = Password.Text;
+
+            string p4ss;
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Mouse");
+            if (key != null)
+            {
+                try
+                {
+                    p4ss = key.GetValue("Name").ToString();
+                    if (pass == p4ss)
+                    {
+                        ScreenApp subWindow1 = new ScreenApp();
+                        subWindow1.Show();
+                        this.Close();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("co cl p4ss");
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("chưa co p4sss");
+                }
+            }
+           
+
+           
+
+
+        }
+        private void ClickClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+      
+
+}
---
+++ SafeKid1/ScreenApp.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SafeKid1
+{
+    /// <summary>
+    /// Interaction logic for MainApp.xaml
+    /// </summary>
+    public partial class ScreenApp : Window
+    {
+        public ScreenApp()
+        {
+            InitializeComponent();
+        }
+        private void Toggle_btn1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ClickClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
