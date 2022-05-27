d04e970475385a18056af63efd6ed05dde3f516a
daoanh123 <37476074+daoanh123@users.noreply.github.com>
2022-05-27 19:44:47 +0700

    add listview

---
+++ SafeKid1/listView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SafeKid1
+{
+    /// <summary>
+    /// Interaction logic for listView.xaml
+    /// </summary>
+    public partial class listView : Window
+    {
+        public listView()
+        {
+            InitializeComponent();
+        }
+    }
+}
